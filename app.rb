f55b3d75d6ccafe5f25d05b29579c1b4763697a2
eoto0 <eoto0@chicagobooth.edu>
2025-04-28 14:59:14 +0000

    final

--- app.rb
+++ app.rb
@@ -52,3 +52,17 @@ get("/dice/5/4") do
 
   erb(:five_four)
 end
+
+get("/dice/:alice/:bob") do
+  @num_dice = params.fetch("alice").to_i
+  @side = params.fetch("bob").to_i
+
+  @rolls = []
+
+  @num_dice.times do
+    die = rand(1..@side)
+    @rolls.push(die)
+  end
+
+  erb(:flexible)
+end
